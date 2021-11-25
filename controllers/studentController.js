cd5abd1f5d13b9c82609042b15104480987d2530
Adboessen <andrewboessen@amdg.rockhursths.edu>
2021-11-25 11:59:48 -0600

    improved routes and added controllers

---
+++ controllers/studentController.js
@@ -0,0 +1,32 @@
+var student = require('./models/students');
+
+//display list of all students
+exports.studentList = function(req, res){
+    res.send('Not implemnted: student list');
+};
+
+//create student
+exports.studentCreateGet = function(req, res){
+    res.send('Not implemnted: student create get');
+};
+exports.studentCreatePost = function(req, res){
+    res.send('Not implemnted: student create post');
+};
+
+//delete student
+exports.studentDeleteGet = function(req, res){
+    res.send('Not implemnted: student delete get');
+};
+exports.studentDeletePost = function(req, res){
+    res.send('Not implemnted: student delete post');
+};
+
+//update student
+exports.studentUpdateGet = function(req, res){
+    res.send('Not implemnted: student update get');
+};
+exports.studentUpdatePost = function(req, res){
+    res.send('Not implemnted: student update post');
+};
+
+
